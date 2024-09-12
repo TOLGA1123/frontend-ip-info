cffaa333a9574787b2d2ebaae153ed22ef6ffbfe
Tolgahan Arslan <90002820+TOLGA1123@users.noreply.github.com>
2024-09-12 10:56:25 +0300

    routes fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import Navbar from './layout/Navbar';
@@ -13,23 +14,46 @@ import PrivateRoute from './util/PrivateRoute';
 import AdminPanel from './Pages/AdminPanel';
 import AllUsers from './Pages/AllUsers';
 function App() {
-  const token = localStorage.getItem('token');
-  const userRole = localStorage.getItem('role');
+  //const token = localStorage.getItem('token');
+  //const userRole = localStorage.getItem('role');
+  const [userRole, setUserRole] = useState(null);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const role = localStorage.getItem('role');
+    
+    if (token) {
+      setUserRole(role);
+    }
+  }, []);
   return (
     <Router>
       <div className="App">
         <Navbar />
         <main>
           <Routes>
+            {/* Home Route*/}
             <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+
+            {/* Login Route - publicly accessible */}
             <Route path="/login" element={<Login />} />
-            <Route path="/register" element={userRole === 'ADMIN' ? (<Register />) : (<Navigate to="/login" />)} />
-            <Route exact path="/addIp" element={<PrivateRoute><AddIp /></PrivateRoute>} />
-            <Route exact path="/viewIp/:id" element={<PrivateRoute><ViewIp /></PrivateRoute>} />
-            <Route exact path="/editIp/:id" element={<PrivateRoute><EditIp /></PrivateRoute>} />
-            <Route path="/admin" element={userRole === 'ADMIN' ? ( <AdminPanel />) : (<Navigate to="/login" />)}/>
+
+            {/* Register Route - accessible only to admins */}
+            <Route path="/register" element={userRole === 'ADMIN' ? <Register /> : <Navigate to="/login" />} />
+
+            {/* Add IP Route*/}
+            <Route path="/addIp" element={<PrivateRoute><AddIp /></PrivateRoute>} />
+
+            {/* View IP Route*/}
+            <Route path="/viewIp/:id" element={<PrivateRoute><ViewIp /></PrivateRoute>} />
+
+            {/* Edit IP Route*/}
+            <Route path="/editIp/:id" element={<PrivateRoute><EditIp /></PrivateRoute>} />
+
+            {/* Admin Panel Route - accessible only to admins */}
+            <Route path="/admin" element={userRole === 'ADMIN' ? <AdminPanel /> : <Navigate to="/login" />} />
+
+            {/* All Users Route*/}
             <Route path="/all-users" element={<PrivateRoute><AllUsers /></PrivateRoute>} />
-            {/* Add more routes here */}
           </Routes>
         </main>
       </div>
